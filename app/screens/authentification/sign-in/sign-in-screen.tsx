e95c577adeee529f2e70ecac853475e5ce779932
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-09 09:37:44 +0100

    components

--- app/screens/authentification/sign-in/sign-in-screen.tsx
+++ app/screens/authentification/sign-in/sign-in-screen.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { TextInput, View, ViewStyle } from "react-native"
-import { AuthInput, Screen, Text } from "../../../components"
+import { AuthInput, Screen, Text, TextField } from "../../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../../theme"
@@ -25,6 +25,9 @@ export const SignInScreen = observer(function SignInScreen() {
         
         <AuthInput textinput="Enter Your username" />
       </SafeAreaView>
+      <TextField
+      //onChangeText
+      />
 
     </Screen>
   
@@ -35,9 +38,4 @@ const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
 }
-const INPUT : ViewStyle = {
-  borderWidth:1,
-  marginHorizontal:metrics.widthPercentageToDP(5),
-  backgroundColor:color.palette.white,
-  borderRadius:20,
-}
+
