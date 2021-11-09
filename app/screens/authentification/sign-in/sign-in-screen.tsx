530daf19dad7dd622e623cf16bc67c0ed1d9c5d4
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-09 11:22:38 +0100

    components

--- app/screens/authentification/sign-in/sign-in-screen.tsx
+++ app/screens/authentification/sign-in/sign-in-screen.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { TextInput, View, ViewStyle } from "react-native"
-import { AuthInput, Button, Screen, Text, TextField } from "../../../components"
+import {TextInput, View, ViewStyle } from "react-native"
+import { AuthInput, Button, Screen, Text, } from "../../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../../theme"
@@ -16,17 +16,39 @@ export const SignInScreen = observer(function SignInScreen() {
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  const [test,setTest]=useState(1);
+  const [userName,setUserName]=useState({
+    name : "ahmed",
+    password :"1111",
+  });
+  useEffect(()=>{
+    console.log(userName)
+  });
   return (
     
     <Screen style={ROOT} preset="scroll">
 
       <SafeAreaView>
         <Text preset="header" text="singin" />
-        
-        <AuthInput textinput="Enter Your useggrname" 
-         
+        <TextInput  
+        placeholder="text 1"
+         onChangeText={(text)=>{userName.name=text}} 
         />
-        
+        <TextInput  
+        placeholder="text 2"
+         onChangeText={(text)=>{userName.name=text}} 
+        />
+        <AuthInput textinput="Enter Your username" 
+          onChangeText={(text)=>{userName.name=text}} 
+          style={AuthenStyle}
+        />
+        <AuthInput textinput="Enter Your password" 
+          onChangeText={(text)=>{userName.password=text}} 
+          style={AuthenStyle}
+        />
+        <Button  text ="SignIn" onPress={()=>setUserName({ name :userName.name,password:userName.password})}/>
+      
+
       </SafeAreaView>
     </Screen>
   
@@ -36,5 +58,8 @@ export const SignInScreen = observer(function SignInScreen() {
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
+}
+const AuthenStyle : ViewStyle={
+  marginVertical:metrics.heightPercentageToDP(1),
 }
 
