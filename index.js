08b29b6e3f5fb880005ace8b59f5654e64ecbf43
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-12 15:31:26 +0100

    initial commit

--- index.js
+++ index.js
@@ -10,5 +10,5 @@
 import App from "./app/app.tsx"
 import { AppRegistry } from "react-native"
 
-AppRegistry.registerComponent("HelloWorld", () => App)
+AppRegistry.registerComponent("App", () => App)
 export default App
